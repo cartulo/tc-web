bcc2bbfaa271cf3b4d8b5c92575a78660c677749
Gilberto Junior <contato.gil.tulo@gmail.com>
2023-08-24 19:26:02 -0300

    feat: Header da aplicação

--- projects/app-web/src/app/layout/app.topbar.component.ts
+++ projects/app-web/src/app/layout/app.topbar.component.ts
@@ -1,20 +1,8 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { MenuItem } from 'primeng/api';
-import { LayoutService } from "projects/app-web/src/app/layout/service/app.layout.service";
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-topbar',
     templateUrl: './app.topbar.component.html'
 })
 export class AppTopBarComponent {
-
-    items!: MenuItem[];
-
-    @ViewChild('menubutton') menuButton!: ElementRef;
-
-    @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
-
-    @ViewChild('topbarmenu') menu!: ElementRef;
-
-    constructor(public layoutService: LayoutService) { }
 }
