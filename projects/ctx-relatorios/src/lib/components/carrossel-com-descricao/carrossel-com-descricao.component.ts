80c0357c62e78e0f71e95e843f575babdbe9f0c0
Gilberto Junior <contato.gil.tulo@gmail.com>
2023-09-10 10:39:45 -0300

    fix :: console.log no código 🤪

--- projects/ctx-relatorios/src/lib/components/carrossel-com-descricao/carrossel-com-descricao.component.ts
+++ projects/ctx-relatorios/src/lib/components/carrossel-com-descricao/carrossel-com-descricao.component.ts
@@ -4,12 +4,9 @@ import {Component, Input, OnInit} from '@angular/core';
     selector: 'ctx-relatorios-carrossel-com-descricao',
     templateUrl: './carrossel-com-descricao.component.html'
 })
-export class CarrosselComDescricaoComponent implements OnInit {
+export class CarrosselComDescricaoComponent {
     @Input() infoDevs: any;
 
     constructor() {}
-    ngOnInit(): void {
-        console.log(this.infoDevs);
-    }
 
 }
